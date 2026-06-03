aa03b359fa161e1bfce05dc5537663fe04531d6e
Michael Castilla <michaelcastilla@gmail.com>
2026-06-02 22:08:14 -0400

    Add robots.txt and wire into webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -81,7 +81,8 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'src/img', to: 'img' }
+        { from: 'src/img', to: 'img' },
+        { from: 'src/robots.txt', to: 'robots.txt' }
       ]
     })
   ]
